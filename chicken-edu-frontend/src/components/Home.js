0a76ef82e9a53260906928d3efbf1f2792dd31bd
EDGARDEVEN <edgardeven303@gmail.com>
2024-06-10 15:37:43 +0300

    Add chicken animation and image to home page

--- chicken-edu-frontend/src/components/Home.js
+++ chicken-edu-frontend/src/components/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Container, Box, Typography, Button, Avatar } from '@mui/material';
+import { Container, Box, Typography, Button, Card, CardMedia } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Lottie from 'react-lottie';
+import animationData from '../animations/chicken.json';
 import './Home.css';
 
 const theme = createTheme();
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
 
 export default function Home() {
     return (
@@ -18,12 +28,10 @@ export default function Home() {
                         alignItems: 'center',
                     }}
                 >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
                     <Typography component="h1" variant="h3" align="center" gutterBottom>
                         Welcome to Chicken Education Gamification System
                     </Typography>
+                    <Lottie options={defaultOptions} height={200} width={200} />
                     <Typography variant="h5" align="center" paragraph>
                         This project is designed to educate users about chickens through a series of interactive quizzes and activities. Use the navigation links to register, log in, and access educational content about chickens.
                     </Typography>
@@ -45,6 +53,14 @@ export default function Home() {
                             Login
                         </Button>
                     </Box>
+                    <Card sx={{ mt: 4 }}>
+                        <CardMedia
+                            component="img"
+                            height="300"
+                            image="https://img.freepik.com/free-vector/white-chicken-skateboard-cartoon-character_1308-89314.jpg?t=st=1718021315~exp=1718024915~hmac=07c1df7a5e8d4b4e2e20929b2222678d4d6811b2db8fdc150a5cb67c69cd23b0&w=740"  // Replace with a valid image URL
+                            alt="Chicken"
+                        />
+                    </Card>
                 </Box>
             </Container>
         </ThemeProvider>
